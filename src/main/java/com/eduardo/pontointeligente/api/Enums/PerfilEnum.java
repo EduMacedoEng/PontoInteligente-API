453058adf35d021717ff5d19bfcb947131098408
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2020-10-01 10:41:37 -0300

    Add suport JPA, MySQL and H2

---
+++ src/main/java/com/eduardo/pontointeligente/api/Enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.eduardo.pontointeligente.api.Enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
+
---
+++ src/main/java/com/eduardo/pontointeligente/api/Enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.eduardo.pontointeligente.api.Enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
